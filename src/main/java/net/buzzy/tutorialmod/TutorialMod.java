6a80475241d7eefa94a637075b61d13c4666a074
Buzgau <buzgau.alexandru@gmail.com>
2024-12-03 17:45:05 +0200

    Added Ruby and Sapphire items, simple stuff

--- src/main/java/net/buzzy/tutorialmod/TutorialMod.java
+++ src/main/java/net/buzzy/tutorialmod/TutorialMod.java
@@ -1,6 +1,10 @@
 package net.buzzy.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.buzzy.tutorialmod.item.ModCreativeModeTabs;
+import net.buzzy.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -24,6 +28,9 @@ public class TutorialMod
     public TutorialMod(FMLJavaModLoadingContext context)
     {
         IEventBus modEventBus = context.getModEventBus();
+        ModCreativeModeTabs.register(modEventBus);
+        ModItems.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
         MinecraftForge.EVENT_BUS.register(this);
         modEventBus.addListener(this::addCreative);
@@ -38,6 +45,10 @@ public class TutorialMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.SAPPHIRE);
+            event.accept(ModItems.RUBY);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
